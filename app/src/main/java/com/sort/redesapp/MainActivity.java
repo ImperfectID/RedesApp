45fe54b3086911e1672016588f5e6538c7ac5ccc
Sortweste <00123216@uca.edu.sv>
2018-04-04 00:51:17 -0600

    Ahora con 32 mascaras aceptables

--- app/src/main/java/com/sort/redesapp/MainActivity.java
+++ app/src/main/java/com/sort/redesapp/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     TextView broadcast;
 
     Pattern p = Pattern.compile("([0-9]|[1-8][0-9]|9[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])");
-    Pattern n = Pattern.compile("(8|16|24|32)");
+    Pattern n = Pattern.compile("([0-9]|[12][0-9]|3[0-2])");
     CharSequence secuencia1 = "Inserte solo valores numericos dentro del rango";
     CharSequence secuencia_mascara = "Inserte una mascara de red valida";
 
@@ -42,7 +44,18 @@ public class MainActivity extends AppCompatActivity {
     int c2;
     int c1;
     int c4;
+    int oct1;
+    int oct2;
+    int oct3;
+    int oct4;
+    int brod_1;
+    int brod_2;
+    int brod_3;
+    int brod_4;
 
+    String cadena2[];
+
+    HashMap<Integer, String>  mascaras_red = new HashMap<Integer, String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +66,38 @@ public class MainActivity extends AppCompatActivity {
         edit3 = findViewById(R.id.tercer);
         edit4 = findViewById(R.id.cuarto);
 
+        mascaras_red.put(1, "128.0.0.0");
+        mascaras_red.put(2, "192.0.0.0");
+        mascaras_red.put(3, "224.0.0.0");
+        mascaras_red.put(4, "240.0.0.0");
+        mascaras_red.put(5, "248.0.0.0");
+        mascaras_red.put(6, "252.0.0.0");
+        mascaras_red.put(7, "254.0.0.0");
+        mascaras_red.put(8, "255.0.0.0");
+        mascaras_red.put(9, "255.128.0.0");
+        mascaras_red.put(10, "255.192.0.0");
+        mascaras_red.put(11, "255.224.0.0");
+        mascaras_red.put(12, "255.240.0.0");
+        mascaras_red.put(13, "255.248.0.0");
+        mascaras_red.put(14, "255.252.0.0");
+        mascaras_red.put(15, "255.254.0.0");
+        mascaras_red.put(16, "255.255.0.0");
+        mascaras_red.put(17, "255.255.128.0");
+        mascaras_red.put(18, "255.255.192.0");
+        mascaras_red.put(19, "255.255.224.0");
+        mascaras_red.put(20, "255.255.240.0");
+        mascaras_red.put(21, "255.255.248.0");
+        mascaras_red.put(22, "255.255.252.0");
+        mascaras_red.put(23, "255.255.254.0");
+        mascaras_red.put(24, "255.255.255.0");
+        mascaras_red.put(25, "255.255.255.128");
+        mascaras_red.put(26, "255.255.255.192");
+        mascaras_red.put(27, "255.255.255.224");
+        mascaras_red.put(28, "255.255.255.240");
+        mascaras_red.put(29, "255.255.255.248");
+        mascaras_red.put(30, "255.255.255.252");
+        mascaras_red.put(31, "255.255.255.254");
+        mascaras_red.put(32, "255.255.255.255");
 
         net = findViewById(R.id.net_id);
         broadcast = findViewById(R.id.broadcast_text);
@@ -78,30 +123,32 @@ public class MainActivity extends AppCompatActivity {
                 } else{
                     mascara.setTextColor(Color.parseColor("#81C784"));
                     int numero = Integer.parseInt(mascara.getText().toString());
-                    numero_mascara=numero;
-                    if ((numero_mascara % 8 == 0) && (numero_mascara > 0)) {
-                        c1 = digito1 & 255;
-                    } else {
-                        c1 = 0;
-                    }
-                    if ((numero_mascara % 8 == 0) && (numero_mascara > 8)) {
-                        c2 = digito2 & 255;
-                    } else {
-                        c2 = 0;
-                    }
-                    if ((numero_mascara % 8 == 0) && (numero_mascara > 16)) {
-                        c3 = digito3 & 255;
-                    } else {
-                        c3 = 0;
-                    }
-                    if ((numero_mascara % 8 == 0) && (numero_mascara > 24)) {
-                        c4 = digito4 & 255;
-                    } else {
-                        c4 = 0;
-                    }
+                    String cadena = mascaras_red.get(numero).toString();
+                    cadena2 = cadena.split("\\.");
+                    oct1 = Integer.parseInt(cadena2[0]);
+                    oct2 = Integer.parseInt(cadena2[1]);
+                    oct3 = Integer.parseInt(cadena2[2]);
+                    oct4 = Integer.parseInt(cadena2[3]);
 
+
+
+                    c1 = digito1 & oct1;
+                    c2 = digito2 & oct2;
+                    c3 = digito3 & oct3;
+                    c4 = digito4 & oct4;
                     net.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
-                    broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + 255);
+
+                    brod_1 = ~oct1 & 0xff;
+                    brod_2 = ~oct2 & 0xff;
+                    brod_3 = ~oct3 & 0xff;
+                    brod_4 = ~oct4 & 0xff;
+                    System.out.println(brod_4);
+
+                    c1 = digito1 | brod_1;
+                    c2 = digito2 | brod_2;
+                    c3 = digito3 | brod_3;
+                    c4 = digito4 | brod_4;
+                    broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
                 }
             }
             @Override
@@ -126,14 +173,23 @@ public class MainActivity extends AppCompatActivity {
                     digito1 = Integer.parseInt(edit1.getText().toString());
                     edit1.setTextColor(Color.parseColor("#81C784"));
 
-                        if ((numero_mascara % 8 == 0) && (numero_mascara > 0)) {
-                            c1 = digito1 & 255;
-                        } else {
-                            c1 = 0;
-                        }
-                        if ((c1 != -1) && (c2 != -1) && (c3 != -1) && (c4 != -1)) {
-                            net.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
-                            broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + 255);
+                    if ((c1 != -1) && (c2 != -1) && (c3 != -1) && (c4 != -1)) {
+                        c1 = digito1 & oct1;
+                        c2 = digito2 & oct2;
+                        c3 = digito3 & oct3;
+                        c4 = digito4 & oct4;
+                        net.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
+
+                        oct1 = ~oct1;
+                        oct2 = ~oct2;
+                        oct3 = ~oct3;
+                        oct4 = ~oct4;
+
+                        c1 = digito1 | oct1;
+                        c2 = digito2 | oct2;
+                        c3 = digito3 | oct3;
+                        c4 = digito4 | oct4;
+                        broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + 255);
                         }
                     }
                 }
@@ -159,14 +215,23 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     digito2 = Integer.parseInt(edit2.getText().toString());
                     edit2.setTextColor(Color.parseColor("#81C784"));
-                    if ((numero_mascara % 8 == 0) && (numero_mascara > 8)) {
-                            c2 = digito2 & 255;
-                        } else {
-                            c2 = 0;
-                        }
-                        if ((c1 != -1) && (c2 != -1) && (c3 != -1) && (c4 != -1)) {
-                            net.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
-                            broadcast.setText(c1+" ."+c2+" ."+c3+" ."+255);
+                    if ((c1 != -1) && (c2 != -1) && (c3 != -1) && (c4 != -1)) {
+                        c1 = digito1 & oct1;
+                        c2 = digito2 & oct2;
+                        c3 = digito3 & oct3;
+                        c4 = digito4 & oct4;
+                        net.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
+
+                        oct1 = ~oct1;
+                        oct2 = ~oct2;
+                        oct3 = ~oct3;
+                        oct4 = ~oct4;
+
+                        c1 = digito1 | oct1;
+                        c2 = digito2 | oct2;
+                        c3 = digito3 | oct3;
+                        c4 = digito4 | oct4;
+                        broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + 255);
                         }
                     }
                 }
@@ -193,14 +258,23 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     digito3 = Integer.parseInt(edit3.getText().toString());
                     edit3.setTextColor(Color.parseColor("#81C784"));
-                        if ((numero_mascara % 8 == 0) && (numero_mascara > 16)) {
-                            c3 = digito3 & 255;
-                        } else {
-                            c3 = 0;
-                        }
-                        if ((c1 != -1) && (c2 != -1) && (c3 != -1) && (c4 != -1)) {
-                            net.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
-                            broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + 255);
+                    if ((c1 != -1) && (c2 != -1) && (c3 != -1) && (c4 != -1)) {
+                        c1 = digito1 & oct1;
+                        c2 = digito2 & oct2;
+                        c3 = digito3 & oct3;
+                        c4 = digito4 & oct4;
+                        net.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
+
+                        oct1 = ~oct1;
+                        oct2 = ~oct2;
+                        oct3 = ~oct3;
+                        oct4 = ~oct4;
+
+                        c1 = digito1 | oct1;
+                        c2 = digito2 | oct2;
+                        c3 = digito3 | oct3;
+                        c4 = digito4 | oct4;
+                        broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + 255);
                         }
                     }
                 }
@@ -227,14 +301,23 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     digito4 = Integer.parseInt(edit4.getText().toString());
                     edit4.setTextColor(Color.parseColor("#81C784"));
-                        if ((numero_mascara % 8 == 0) && (numero_mascara > 24)) {
-                            c4 = digito4 & 255;
-                        } else {
-                            c4 = 0;
-                        }
-                        if ((c1 != -1) && (c2 != -1) && (c3 != -1) && (c4 != -1)) {
-                            net.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
-                            broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + 255);
+                    if ((c1 != -1) && (c2 != -1) && (c3 != -1) && (c4 != -1)) {
+                        c1 = digito1 & oct1;
+                        c2 = digito2 & oct2;
+                        c3 = digito3 & oct3;
+                        c4 = digito4 & oct4;
+                        net.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
+
+                        oct1 = ~oct1;
+                        oct2 = ~oct2;
+                        oct3 = ~oct3;
+                        oct4 = ~oct4;
+
+                        c1 = digito1 | oct1;
+                        c2 = digito2 | oct2;
+                        c3 = digito3 | oct3;
+                        c4 = digito4 | oct4;
+                        broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + 255);
                         }
                     }
 
