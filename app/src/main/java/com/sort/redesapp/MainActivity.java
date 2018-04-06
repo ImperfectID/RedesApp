d27efe2f4ac152c82c88a264f8bc245ecc1710ef
petrlr14 <00057616@uca.edu.sv>
2018-04-05 20:03:38 -0600

    se modulizaron acciones y se libero el metodo onCreate

--- app/src/main/java/com/sort/redesapp/MainActivity.java
+++ app/src/main/java/com/sort/redesapp/MainActivity.java
@@ -5,13 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -37,24 +33,11 @@ public class MainActivity extends AppCompatActivity {
     CharSequence secuencia1 = "Inserte solo valores numericos dentro del rango";
     CharSequence secuencia_mascara = "Inserte una mascara de red valida";
 
-    int digito1;
-    int digito2;
-    int digito3;
-    int digito4;
+    int digito1,digito2,digito3,digito4;
     int numero_mascara;
-    int cont = 0;
-    int c3;
-    int c2;
-    int c1;
-    int c4;
-    int oct1;
-    int oct2;
-    int oct3;
-    int oct4;
-    int brod_1;
-    int brod_2;
-    int brod_3;
-    int brod_4;
+    int c1=-1,c2=-1,c3=-1,c4=-1;
+    int oct1, oct2, oct3, oct4;
+    int brod_1, brod_2, brod_3, brod_4;
 
     String cadena2[];
 
@@ -64,11 +47,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edit1 = findViewById(R.id.primer);
-        edit2 = findViewById(R.id.segundo);
-        edit3 = findViewById(R.id.tercer);
-        edit4 = findViewById(R.id.cuarto);
+        gettingStarted();
+        settingListeners();
+    }
 
+    //handler en donde se llaman todos los metodos de inicializacion
+    public void gettingStarted(){
+        c1=-1;c2=-1;c3=-1;c4=-1;
+        numero_mascara=32;
+        gettingResourses();
+        settingNetMask();
+
+    }
+
+    //Se setean los valores de net mask segun bits utilizados
+    private void settingNetMask(){
         mascaras_red.put(1, "128.0.0.0");
         mascaras_red.put(2, "192.0.0.0");
         mascaras_red.put(3, "224.0.0.0");
@@ -101,19 +94,24 @@ public class MainActivity extends AppCompatActivity {
         mascaras_red.put(30, "255.255.255.252");
         mascaras_red.put(31, "255.255.255.254");
         mascaras_red.put(32, "255.255.255.255");
+    }
 
+
+    //Metodo para obtener los recursos visuales
+    private void gettingResourses(){
+        edit1 = findViewById(R.id.primer);
+        edit2 = findViewById(R.id.segundo);
+        edit3 = findViewById(R.id.tercer);
+        edit4 = findViewById(R.id.cuarto);
         net = findViewById(R.id.net_id);
         broadcast = findViewById(R.id.broadcast_text);
         red_utilizable = findViewById(R.id.cantidad_text);
         host_number = findViewById(R.id.host_text);
         red_number = findViewById(R.id.red_text);
-
         mascara = findViewById(R.id.mascara);
+    }
 
-        c1=-1; c2=-1; c3=-1; c4=-1;
-
-        numero_mascara=32;
-
+    private void settingListeners(){
         mascara.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -131,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
                     int numero = Integer.parseInt(mascara.getText().toString());
                     int redVerificador = (int)Math.pow(2,32-numero)-2;
                     red_utilizable.setText(redVerificador+"");
-                    String cadena = mascaras_red.get(numero).toString();
+                    String cadena = mascaras_red.get(numero);
                     cadena2 = cadena.split("\\.");
                     oct1 = Integer.parseInt(cadena2[0]);
                     oct2 = Integer.parseInt(cadena2[1]);
@@ -165,8 +163,6 @@ public class MainActivity extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
             }
         });
-
-
         edit1.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -204,8 +200,8 @@ public class MainActivity extends AppCompatActivity {
                         red_number.setText(digito1+". "+digito2);
                         host_number.setText(digito3+" ."+digito4);
                     }
-                    }
                 }
+            }
 
 
             @Override
@@ -248,9 +244,9 @@ public class MainActivity extends AppCompatActivity {
                         broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
                         red_number.setText(digito1+". "+digito2);
                         host_number.setText(digito3+" ."+digito4);
-                        }
                     }
                 }
+            }
 
 
 
@@ -294,9 +290,9 @@ public class MainActivity extends AppCompatActivity {
                         broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
                         red_number.setText(digito1+". "+digito2);
                         host_number.setText(digito3+" ."+digito4);
-                        }
                     }
                 }
+            }
 
 
             @Override
@@ -339,8 +335,8 @@ public class MainActivity extends AppCompatActivity {
                         broadcast.setText(c1 + " ." + c2 + " ." + c3 + " ." + c4);
                         red_number.setText(digito1+". "+digito2);
                         host_number.setText(digito3+" ."+digito4);
-                        }
                     }
+                }
 
             }
 
